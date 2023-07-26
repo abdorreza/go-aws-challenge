95237d7484504736f6442b9af935ae13bf66ba33
unknown <joe.afshany@gmail.com>
2023-07-26 13:14:16 +0330

    add some code in main.go

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+/*func HandleRequest(ctx context.Context, event interface{}) (string, error) {
+	fmt.Printf("%v\n", event)
+	return "Hello from Lambda!", nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}*/
+
 func main() {
 	lambda.Start(device.Handler)
 }
